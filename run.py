484dc5d1160a0c751c8c2f10bad224c9567e7629
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-01-17 22:05:28 +0000

    Added contact page and updated python code

--- run.py
+++ run.py
@@ -5,8 +5,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World"
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
