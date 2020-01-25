c7a86e81b54b6322067ea26d8931e262ba044d88
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-01-25 11:58:47 +0000

    added template for about sections on each member

--- run.py
+++ run.py
@@ -17,6 +17,18 @@ def about():
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data, list_of_numbers=[1, 2, 3])
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"]  == member_name:
+                member = obj
+
+    return render_template("member.html", member=member)
+
 
 @app.route('/contact')
 def contact():
